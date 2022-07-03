d95e26fc8877e4c98feee69f74c2aa2ca93ab49a
The73756 <the73756the@gmail.com>
2022-07-03 17:11:47 +0900

    add main slider on first page

---
+++ src/js/modules/slider.js
@@ -0,0 +1,44 @@
+export default class Slider {
+	constructor(page, btns) {
+		this.page = document.querySelector(page);
+		this.slides = this.page.children;
+		this.btns = document.querySelectorAll(btns);
+		this.slideIndex = 1;
+	}
+
+	showSlides(n) {
+		if (n > this.slides.length) {
+			this.slideIndex = 1;
+		} 
+		
+		if (n < 1) {
+			this.slideIndex = this.slides.length;
+		}
+
+		this.slides.forEach(slide => {
+			slide.classList.remove('show-slides');
+		});
+
+		this.slides[this.slideIndex -1].classList.add('show-slides');
+	}
+
+	plusSlides(n) {
+		this.showSlides(this.slideIndex += n);
+	}
+
+	render() {
+		this.btns.forEach(el => {
+			el.addEventListener('click', () => {
+				this.plusSlides(1);
+			});
+
+			el.parentNode.previousElementSibling.addEventListener('click', (e) => {
+				e.preventDefault();
+				this.slideIndex = 1;
+				this.showSlides(this.slideIndex);
+			});
+		});
+
+		this.showSlides(this.slideIndex);
+	}
+}
